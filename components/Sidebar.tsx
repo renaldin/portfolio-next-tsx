08b697add0814e015598f74b9941d055f4f17d55
renaldinoviandi0@gmail.com <renaldinoviandi4@gmail.com>
2022-12-15 07:32:40 +0700

    update sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
         <div>
             <img src="https://sumitdey.netlify.app/static/media/max.9d3a6d3e.jpg" className='w-32 h-32 rounded-full mx-auto' alt="Avatar" />
             <h3 className='my-4 text-3xl font-medium tracking-wider font-kaushan'>
-                <span>Renaldi </span>
+                <span className='text-green'>Renaldi </span>
                 Noviandi
             </h3>
             <p className='px-2 py-1 my-3 bg-gray-200 rounded-full'>Web Developer</p>
@@ -15,7 +15,7 @@ const Sidebar = () => {
                 <GiTie className='w-6 h-6' /> Download Resume
             </a>
             {/* Social Icon */}
-            <div className='flex justify-around my-5 text-green-500 w-9/12 md:w-full mx-auto'>
+            <div className='flex justify-around my-5 text-green w-9/12 md:w-full mx-auto'>
                 <a href="">
                     <AiFillGithub className='w-8 h-8 cursor-pointer' />
                 </a>
@@ -37,8 +37,10 @@ const Sidebar = () => {
                 <p className='my-2'>0898989786758</p>
             </div>
             {/* Email Button */}
-            <button className='bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 my-2 text-white'>Email Me</button>
-            <button className='bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 my-2 text-white'>Toggle Theme</button>
+            <button className='bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 my-2 text-white focus:outline-none' onClick={() => window.open('mailto:renaldinoviandi9@gmail.com')}>
+                Email Me
+            </button>
+            <button className='bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 my-2 text-white'>Toggle Theme</button>
         </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import Sidebar from '../components/Sidebar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<div className='grid grid-cols-12 gap-6 my-14 lg:px-48 px-5'>
+  return (<div className='grid grid-cols-12 gap-6 my-14 lg:px-48 px-5 sm:px-20 md:px-32'>
     <div className='col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl'>
       <Sidebar />
     </div>
