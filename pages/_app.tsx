5042fe481395e8b57eb30d345012600a909167b9
renaldinoviandi0@gmail.com <renaldinoviandi4@gmail.com>
2022-12-16 07:48:39 +0700

    update nav item

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<div className='grid grid-cols-12 gap-6 my-14 lg:px-48 px-5 sm:px-20 md:px-32'>
     <div className='col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl'>
       <Sidebar />
     </div>
-    <div className='col-span-12 bg-white lg:col-span-9 rounded-2xl'>
+    <div className='col-span-12 bg-white lg:col-span-9 rounded-2xl flex flex-col'>
+      <Navbar />
       <Component {...pageProps} />
     </div>
   </div>
---
+++ pages/projects.tsx
@@ -0,0 +1,9 @@
+const projects = () => {
+    return (
+        <div className="p-4">
+            Project Page
+        </div>
+    )
+}
+
+export default projects
