3908c52b0c234c21a92023ad54029fbe520ec611
renaldin <renaldinoviandi0@gmail.com>
2023-01-07 21:09:06 +0700

    add motion in bar

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -1,15 +1,32 @@
 import { FunctionComponent } from "react"
 import { ISkill } from "../type"
+import { motion } from 'framer-motion'
 
 const Bar: FunctionComponent<{
     data: ISkill
 }> = ({ data: { Icon, level, name } }) => {
+
+    const variants = {
+        initial: {
+            width: 0
+        },
+        animate: {
+            width: level,
+            transition: {
+                duration: 0.4,
+                type: "spring",
+                damping: 10,
+                stiffness: 100
+            }
+        },
+    }
+
     return (
         <div className="text-white my-2 bg-gray-300 rounded-full dark:bg-dark-300">
-            <div className="px-4 py-1 flex items-center rounded-full bg-gradient-to-r from-green to-blue-600" style={{ width: level }}>
+            <motion.div className="px-4 py-1 flex items-center rounded-full bg-gradient-to-r from-green to-blue-600" style={{ width: level }} variants={variants} initial="initial" animate="animate">
                 <Icon className="mr-3" />
                 {name}
-            </div>
+            </motion.div>
         </div>
     )
 }
