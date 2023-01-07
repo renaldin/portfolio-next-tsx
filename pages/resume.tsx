de619016cd85d13aee780c1a18a3e9e38dddb20b
renaldin <renaldinoviandi0@gmail.com>
2023-01-07 21:23:04 +0700

    add motion resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,13 +1,16 @@
 import Bar from "../components/Bar"
 import { languages, tools } from "../data"
+import { motion } from 'framer-motion'
+import { fadeInUp } from "../animations"
 
 const resume = () => {
+
     return (
         <div className="px-6 py-2">
             {/* education & exp */}
 
             <div className="grid gap-6 md:grid-cols-2">
-                <div>
+                <motion.div variants={fadeInUp} initial="initial" animate="animate">
                     <h5 className="my-3 text-2xl font-bold">Education</h5>
                     <div>
                         <h5 className="my-2 text-xl font-bold">Computer Science Engineering</h5>
@@ -16,8 +19,8 @@ const resume = () => {
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non quam error vel facere. Delectus ratione architecto mollitia nisi.
                         </p>
                     </div>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div variants={fadeInUp} initial="initial" animate="animate">
                     <h5 className="my-3 text-2xl font-bold">Experience</h5>
                     <div>
                         <h5 className="my-2 text-xl font-bold">Software Engineer Jr.</h5>
@@ -26,7 +29,7 @@ const resume = () => {
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                         </p>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
             {/* langauges & tools */}
